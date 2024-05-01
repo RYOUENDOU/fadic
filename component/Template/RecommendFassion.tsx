a33033507ca2e384627ac18a0f53edcf8623c723
git-4606-ikawa <ikawa.takafumi.4606@members.co.jp>
2024-05-01 17:27:55 +0900

    おすすめ写真の下のアイコン修正

--- component/Template/RecommendFassion.tsx
+++ component/Template/RecommendFassion.tsx
@@ -5,32 +5,53 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Box, Typography, css } from "@mui/material";
-import { FC } from "react";
-import { Android, Clear, CoPresent } from "@mui/icons-material";
+import React, { FC } from "react";
+import {
+  AdfScanner,
+  AllInclusive,
+  Android,
+  Clear,
+} from "@mui/icons-material";
 import { RecommendPictureInfo } from "../Types/RecommendPictureInfo";
 
 // reccomend画像の一覧。実際にはDBから取得
 const images = ["/imageCheck.png", "/zoro.png", "/imageCheck.png"];
 const topsColor: string = "yellow";
 
-const recommendPictureList: RecommendPictureInfo[] = [{
-  image: "/imageCheck.png",
-  topsColor: "white",
-  bottomsType: "bottoms",
-  bottomsColor: "black"
-},{
-  image: "/zoro.png",
-  topsColor: "red",
-  bottomsType: "skirt",
-  bottomsColor: "red"
-},{
-  image: "/imageCheck.png",
-  topsColor: "green",
-  bottomsType: "bottoms",
-  bottomsColor: "white"
-}]
+const recommendPictureList: RecommendPictureInfo[] = [
+  {
+    image: "/imageCheck.png",
+    topsColor: "yellow",
+    bottomsType: "tops",
+    bottomsColor: "black",
+  },
+  {
+    image: "/zoro.png",
+    topsColor: "gray",
+    bottomsType: "skirt",
+    bottomsColor: "red",
+  },
+  {
+    image: "/imageCheck.png",
+    topsColor: "green",
+    bottomsType: "bottoms",
+    bottomsColor: "blue",
+  },
+];
 
 const RecommendFassion: FC = () => {
+  const displayTops = (color: string): React.ReactNode => {
+    return <Android fontSize="large" style={{ color: color }} />;
+  };
+
+  const displayBottoms = (type: string, color: string): React.ReactNode => {
+    if ("bottoms" == type) {
+      return <AdfScanner fontSize="large" style={{ color: color }} />;
+    } else {
+      return <AllInclusive fontSize="large" style={{ color: color }} />;
+    }
+  };
+
   return (
     // 横幅を端末に応じて可変させたい
     <Box
@@ -39,7 +60,6 @@ const RecommendFassion: FC = () => {
         justifyContent: "center",
         alignItems: "center",
         marginTop: "5%",
-
       }}
     >
       <Swiper
@@ -49,53 +69,68 @@ const RecommendFassion: FC = () => {
           clickable: true,
         }}
         loop={true}
-        // autoplay={{
-        //   delay: 2500,
-        //   disableOnInteraction: false,
-        // }}
         style={{ justifyContent: "center", alignItems: "center" }}
       >
-        {images.map((src: string, index: number) => {
-          return (
-            // styleでcenter指定することで画像を真ん中に配置できた
-            <SwiperSlide
-              key={`${index}`}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Box>
-                {/* 画像の下のアイコンを下の方に配置 */}
-                <Image src={src} width={330} height={330} alt="test_image" />
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexFlow: "column",
-                    marginBottom: "50px",
-                  }}
-                >
+        {recommendPictureList.map(
+          (pictureInfo: RecommendPictureInfo, index: number) => {
+            return (
+              // styleでcenter指定することで画像を真ん中に配置できた
+              <SwiperSlide
+                key={`${index}`}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Box>
+                  <Image
+                    src={pictureInfo.image}
+                    width={330}
+                    height={330}
+                    alt="test_image"
+                  />
+                  {/* 画像の下のアイコンを下の方に配置 */}
                   <Box
                     sx={{
                       display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      backgroundColor: "white",
-                      height: "50px",
+                      flexFlow: "column",
+                      marginBottom: "50px",
                     }}
                   >
-                    <Box sx={{display: "flex"}}>
-                    <Android fontSize="large" />
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        backgroundColor: "white",
+                        height: "50px",
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          display: "flex",
+                          justifyContent: "space-around",
+                          width: "120px",
+                        }}
+                      >
+                        {displayTops(pictureInfo.topsColor)}
+                        {/* <Android fontSize="large" /> */}
+                        <Box paddingTop={1}>
+                          <Clear color="disabled" fontSize="small" />
+                        </Box>
+                        {displayBottoms(
+                          pictureInfo.bottomsType,
+                          pictureInfo.bottomsColor
+                        )}
+                      </Box>
                     </Box>
-                    <Clear color="disabled" fontSize="small" />
-                    <CoPresent fontSize="large" />
                   </Box>
                 </Box>
-              </Box>
-            </SwiperSlide>
-          );
-        })}
+              </SwiperSlide>
+            );
+          }
+        )}
       </Swiper>
     </Box>
   );
